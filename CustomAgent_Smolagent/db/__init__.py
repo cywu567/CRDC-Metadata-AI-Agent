0f521f80270761ced172983e327296fe66ce59f0
celinewu <cywu@caltech.edu>
2025-07-21 15:37:00 -0400

    cleaning up

--- CustomAgent_Smolagent/db/__init__.py
+++
@@ -1,9 +0,0 @@
-
-#from .db import (
-#    connect,
-#    init_schema,
-#    save_submission,
-#    log_feedback,
-#)
-
-#__all__ = ["connect", "init_schema", "save_submission", "log_feedback"]
--- CustomAgent_Smolagent/tools/__init__.py
+++
@@ -1,2 +0,0 @@
-#from .log_feedback import LogFeedbackTool
-#__all__.append("LogFeedbackTool")
